127421d084435d33fb1c8840e115a5f1b1138cf3
Nahid Islam <nahidshaiket10300@gmail.com>
2022-04-09 08:58:36 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{padding: "0 100px 0 100px"}}>
+      <FullCalendar
+        themeSystem="Lux"
+        fixedWeekCount={false}
+        locale="de"
+        headerToolbar={
+          {
+            left: 'prev',
+            center: 'title',
+            right: "next"
+          }
+        }
+        plugins={[ dayGridPlugin ]}
+        initialView="dayGridMonth"
+        aspectRatio="1.7"
+        events={[
+          { title: 'event 1', date: '2022-04-07', color: '#44619A', textColor: 'white' },
+          { title: 'event 3', date: '2022-04-07', color: '#E87E4F', textColor: 'white' },
+          { title: 'event 2', date: '2022-04-08', color: '#F6B229', textColor: 'white'  }
+        ]}
+      />
     </div>
   );
 }
